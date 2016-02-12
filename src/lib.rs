a46e8aee42335b9a24967a36457436be69932a92
Ben S <ogham@bsago.me>
2016-02-12 18:30:57 +0000

    Add the basic outline to the documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,31 @@
-//! Parsing Olson DB formats.
+//! Rust library for reading the text files comprising the [zoneinfo
+//! database][w], which records time zone changes and offsets across the world
+//! from multiple sources.
+//!
+//! The zoneinfo database is distributed in one of two formats: a raw text
+//! format with one file per continent, and a compiled binary format with one
+//! file per time zone. This crate deals with the former; for the latter, see
+//! the [`zoneinfo_compiled` crate][zc] instead.
+//!
+//! The database itself is maintained by IANA. For more information, see
+//! [IANA’s page on the time zone database][iana]. You can also find the text
+//! files themselves in [the tz repository][tz].
+//!
+//! [iana]: https://www.iana.org/time-zones
+//! [tz]: https://github.com/eggert/tz
+//! [w]: https://en.wikipedia.org/wiki/Tz_database
+//! [zc]: https://datetime.rustdocs.org/zoneinfo_compiled/index.html
+//!
+//! ## Outline
+//!
+//! Reading a zoneinfo text file is split into three stages:
+//!
+//! - **Parsing** individual lines of text into `Lines` is done by the `line`
+//!   module;
+//! - **Interpreting** these lines into a complete `Table` is done by the
+//!   `table` module;
+//! - **Calculating transitions** from this table is done by the `transitions`
+//!   module.
 
 #![crate_name = "zoneinfo_parse"]
 #![crate_type = "rlib"]
