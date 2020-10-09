d1f8a79be4723e76947f60169706dbb563bb16e1
Brandon W Maister <quodlibetor@gmail.com>
2020-10-09 09:03:25 -0400

    clippy: Fix unwrap_or -> unwrap_or_else

--- build.rs
+++ build.rs
@@ -302,7 +302,7 @@ fn main() {
     let lines = tzfiles
         .iter()
         .map(Path::new)
-        .map(|p| Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap_or("".to_string())).join(p))
+        .map(|p| Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap_or_else(String::new)).join(p))
         .map(File::open)
         .map(Result::unwrap)
         .map(BufReader::new)
