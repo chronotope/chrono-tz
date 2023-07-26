3c3f0cc57e2ab229463e3d09419dbe0a9a2a6be4
Paul Dicker <pitdicker@gmail.com>
2023-07-26 10:01:00 +0200

    Avoid deprecated methods in `tests/numberphile`

--- chrono-tz/tests/numberphile.rs
+++ chrono-tz/tests/numberphile.rs
@@ -6,7 +6,7 @@
 //!
 //! [video]: https://www.youtube.com/watch?v=-5wpm-gesOY
 
-use chrono::{DateTime, TimeZone};
+use chrono::{DateTime, NaiveDate, TimeZone};
 
 use chrono_tz::Africa::Tripoli;
 use chrono_tz::America::New_York;
@@ -25,8 +25,8 @@ fn seconds<Tz1: TimeZone, Tz2: TimeZone>(from: DateTime<Tz1>, to: DateTime<Tz2>)
 
 #[test]
 fn london_5_days_ago_to_new_york() {
-    let from = London.ymd(2013, 12, 25).and_hms(14, 0, 0);
-    let to = New_York.ymd(2013, 12, 30).and_hms(14, 0, 0);
+    let from = London.with_ymd_and_hms(2013, 12, 25, 14, 0, 0).unwrap();
+    let to = New_York.with_ymd_and_hms(2013, 12, 30, 14, 0, 0).unwrap();
     assert_eq!(seconds(from, to), 60 * 60 * (24 * 5 + 5));
 }
 
@@ -34,74 +34,74 @@ fn london_5_days_ago_to_new_york() {
 fn london_to_australia() {
     // at the time Tom was speaking, Adelaide was 10 1/2 hours ahead
     // many other parts of Australia use different time zones
-    let from = London.ymd(2013, 12, 25).and_hms(14, 0, 0);
-    let to = Adelaide.ymd(2013, 12, 30).and_hms(14, 0, 0);
+    let from = London.with_ymd_and_hms(2013, 12, 25, 14, 0, 0).unwrap();
+    let to = Adelaide.with_ymd_and_hms(2013, 12, 30, 14, 0, 0).unwrap();
     assert_eq!(seconds(from, to), 60 * 60 * (24 * 5 - 10) - 60 * 30);
 }
 
 #[test]
 fn london_to_nepal() {
     // note Tom gets this wrong, it's 5 3/4 hours as he is speaking
-    let from = London.ymd(2013, 12, 25).and_hms(14, 0, 0);
-    let to = Kathmandu.ymd(2013, 12, 30).and_hms(14, 0, 0);
+    let from = London.with_ymd_and_hms(2013, 12, 25, 14, 0, 0).unwrap();
+    let to = Kathmandu.with_ymd_and_hms(2013, 12, 30, 14, 0, 0).unwrap();
     assert_eq!(seconds(from, to), 60 * 60 * (24 * 5 - 5) - 60 * 45);
 }
 
 #[test]
 fn autumn() {
-    let from = London.ymd(2013, 10, 25).and_hms(12, 0, 0);
-    let to = London.ymd(2013, 11, 1).and_hms(12, 0, 0);
+    let from = London.with_ymd_and_hms(2013, 10, 25, 12, 0, 0).unwrap();
+    let to = London.with_ymd_and_hms(2013, 11, 1, 12, 0, 0).unwrap();
     assert_eq!(seconds(from, to), 60 * 60 * (24 * 7 + 1));
 }
 
 #[test]
 fn earlier_daylight_savings_in_new_york() {
-    let from = New_York.ymd(2013, 10, 25).and_hms(12, 0, 0);
-    let to = New_York.ymd(2013, 11, 1).and_hms(12, 0, 0);
+    let from = New_York.with_ymd_and_hms(2013, 10, 25, 12, 0, 0).unwrap();
+    let to = New_York.with_ymd_and_hms(2013, 11, 1, 12, 0, 0).unwrap();
     assert_eq!(seconds(from, to), 60 * 60 * (24 * 7));
 }
 
 #[test]
 fn southern_hemisphere_clocks_forward() {
-    let from = Adelaide.ymd(2013, 10, 1).and_hms(12, 0, 0);
-    let to = Adelaide.ymd(2013, 11, 1).and_hms(12, 0, 0);
+    let from = Adelaide.with_ymd_and_hms(2013, 10, 1, 12, 0, 0).unwrap();
+    let to = Adelaide.with_ymd_and_hms(2013, 11, 1, 12, 0, 0).unwrap();
     assert_eq!(seconds(from, to), 60 * 60 * (24 * 31 - 1));
 }
 
 #[test]
 fn samoa_skips_a_day() {
-    let from = Apia.ymd(2011, 12, 29).and_hms(12, 0, 0);
-    let to = Apia.ymd(2011, 12, 31).and_hms(12, 0, 0);
+    let from = Apia.with_ymd_and_hms(2011, 12, 29, 12, 0, 0).unwrap();
+    let to = Apia.with_ymd_and_hms(2011, 12, 31, 12, 0, 0).unwrap();
     assert_eq!(seconds(from, to), 60 * 60 * 24);
 }
 
 #[test]
 fn double_bst() {
-    let from = London.ymd(1942, 6, 1).and_hms(12, 0, 0);
-    let to = UTC.ymd(1942, 6, 1).and_hms(12, 0, 0);
+    let from = London.with_ymd_and_hms(1942, 6, 1, 12, 0, 0).unwrap();
+    let to = UTC.with_ymd_and_hms(1942, 6, 1, 12, 0, 0).unwrap();
     assert_eq!(seconds(from, to), 60 * 60 * 2);
 }
 
 #[test]
 fn libya_2013() {
     // Libya actually put their clocks *forward* in 2013, but not in any other year
-    let from = Tripoli.ymd(2012, 3, 1).and_hms(12, 0, 0);
-    let to = Tripoli.ymd(2012, 4, 1).and_hms(12, 0, 0);
+    let from = Tripoli.with_ymd_and_hms(2012, 3, 1, 12, 0, 0).unwrap();
+    let to = Tripoli.with_ymd_and_hms(2012, 4, 1, 12, 0, 0).unwrap();
     assert_eq!(seconds(from, to), 60 * 60 * 24 * 31);
 
-    let from = Tripoli.ymd(2013, 3, 1).and_hms(12, 0, 0);
-    let to = Tripoli.ymd(2013, 4, 1).and_hms(12, 0, 0);
+    let from = Tripoli.with_ymd_and_hms(2013, 3, 1, 12, 0, 0).unwrap();
+    let to = Tripoli.with_ymd_and_hms(2013, 4, 1, 12, 0, 0).unwrap();
     assert_eq!(seconds(from, to), 60 * 60 * (24 * 31 - 1));
 
-    let from = Tripoli.ymd(2014, 3, 1).and_hms(12, 0, 0);
-    let to = Tripoli.ymd(2014, 4, 1).and_hms(12, 0, 0);
+    let from = Tripoli.with_ymd_and_hms(2014, 3, 1, 12, 0, 0).unwrap();
+    let to = Tripoli.with_ymd_and_hms(2014, 4, 1, 12, 0, 0).unwrap();
     assert_eq!(seconds(from, to), 60 * 60 * 24 * 31);
 }
 
 #[test]
 fn israel_palestine() {
-    let from = Jerusalem.ymd(2016, 10, 29).and_hms(12, 0, 0);
-    let to = Gaza.ymd(2016, 10, 29).and_hms(12, 0, 0);
+    let from = Jerusalem.with_ymd_and_hms(2016, 10, 29, 12, 0, 0).unwrap();
+    let to = Gaza.with_ymd_and_hms(2016, 10, 29, 12, 0, 0).unwrap();
     assert_eq!(seconds(from, to), 60 * 60);
 }
 
@@ -109,8 +109,8 @@ fn israel_palestine() {
 #[test]
 #[ignore]
 fn london_julian_to_gregorian() {
-    let from = London.ymd(1752, 9, 2).and_hms(12, 0, 0);
-    let to = London.ymd(1752, 9, 14).and_hms(12, 0, 0);
+    let from = London.with_ymd_and_hms(1752, 9, 2, 12, 0, 0).unwrap();
+    let to = London.with_ymd_and_hms(1752, 9, 14, 12, 0, 0).unwrap();
     assert_eq!(seconds(from, to), 60 * 60 * 24);
 }
 
@@ -118,8 +118,8 @@ fn london_julian_to_gregorian() {
 #[test]
 #[ignore]
 fn russian_julian_to_gregorian() {
-    let from = Moscow.ymd(1918, 1, 31).and_hms(12, 0, 0);
-    let to = Moscow.ymd(1918, 2, 14).and_hms(12, 0, 0);
+    let from = Moscow.with_ymd_and_hms(1918, 1, 31, 12, 0, 0).unwrap();
+    let to = Moscow.with_ymd_and_hms(1918, 2, 14, 12, 0, 0).unwrap();
     assert_eq!(seconds(from, to), 60 * 60 * 24);
 }
 
@@ -127,15 +127,17 @@ fn russian_julian_to_gregorian() {
 #[test]
 #[ignore]
 fn london_25_march() {
-    let from = London.ymd(924, 3, 24).and_hms(12, 0, 0);
-    let to = London.ymd(925, 3, 25).and_hms(12, 0, 0);
+    let from = London.with_ymd_and_hms(924, 3, 24, 12, 0, 0).unwrap();
+    let to = London.with_ymd_and_hms(925, 3, 25, 12, 0, 0).unwrap();
     assert_eq!(seconds(from, to), 60 * 60 * 24);
 }
 
 #[test]
 fn leapsecond() {
-    let from = UTC.ymd(2016, 6, 30).and_hms(23, 59, 59);
-    let to = UTC.ymd(2016, 6, 30).and_hms_milli(23, 59, 59, 1000);
+    let from = UTC.with_ymd_and_hms(2016, 6, 30, 23, 59, 59).unwrap();
+    let leap =
+        NaiveDate::from_ymd_opt(2016, 6, 30).unwrap().and_hms_milli_opt(23, 59, 59, 1000).unwrap();
+    let to = UTC.from_local_datetime(&leap).unwrap();
     assert_eq!(seconds(from, to), 1);
 }
 
@@ -143,8 +145,8 @@ fn leapsecond() {
 #[test]
 #[ignore]
 fn leapsecond_2() {
-    let from = UTC.ymd(2016, 6, 30).and_hms(23, 59, 59);
-    let to = UTC.ymd(2016, 7, 1).and_hms(0, 0, 0);
+    let from = UTC.with_ymd_and_hms(2016, 6, 30, 23, 59, 59).unwrap();
+    let to = UTC.with_ymd_and_hms(2016, 7, 1, 0, 0, 0).unwrap();
     assert_eq!(seconds(from, to), 2);
 }
 
@@ -152,7 +154,9 @@ fn leapsecond_2() {
 #[test]
 #[ignore]
 fn leapsecond_3() {
-    let from = UTC.ymd(2016, 6, 30).and_hms_milli(23, 59, 59, 1000);
-    let to = UTC.ymd(2016, 7, 1).and_hms(0, 0, 0);
+    let leap =
+        NaiveDate::from_ymd_opt(2016, 6, 30).unwrap().and_hms_milli_opt(23, 59, 59, 1000).unwrap();
+    let from = UTC.from_local_datetime(&leap).unwrap();
+    let to = UTC.with_ymd_and_hms(2016, 7, 1, 0, 0, 0).unwrap();
     assert_eq!(seconds(from, to), 1);
 }
