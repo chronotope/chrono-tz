784d4842f08b4e0b4203592c03298d73bbe13b98
Dirkjan Ochtman <dirkjan@ochtman.nl>
2024-05-06 13:37:12 +0200

    parse-zoneinfo: add snapshot tests

---
+++ parse-zoneinfo/tests/snapshot.rs
@@ -0,0 +1,30 @@
+use std::error::Error;
+
+use insta::assert_debug_snapshot;
+
+use parse_zoneinfo::line::{Line, LineParser};
+use parse_zoneinfo::FILES;
+
+#[ignore]
+#[test]
+fn test_parse() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+    let mut files = vec![];
+    let parser = LineParser::default();
+    for file in FILES {
+        let path = format!("../chrono-tz/tz/{file}");
+        let text = std::fs::read_to_string(&path)?;
+        let mut lines = vec![];
+        for ln in text.lines() {
+            dbg!(ln);
+            match parser.parse_str(ln)? {
+                Line::Space => continue,
+                ln => lines.push(format!("{ln:?}")),
+            }
+        }
+
+        files.push((file, lines));
+    }
+
+    assert_debug_snapshot!(files);
+    Ok(())
+}
