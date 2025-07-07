59e2ed4d60e0f1f22f479cf5a433b7b97c2c904b
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-07-07 15:28:57 +0200

    Move build script message printing out of library

--- chrono-tz/build.rs
+++ chrono-tz/build.rs
@@ -1,3 +1,8 @@
+#[cfg(feature = "filter-by-regex")]
+use chrono_tz_build::FILTER_ENV_VAR_NAME;
+
 fn main() {
+    #[cfg(feature = "filter-by-regex")]
+    println!("cargo:rerun-if-env-changed={FILTER_ENV_VAR_NAME}");
     chrono_tz_build::main();
 }
